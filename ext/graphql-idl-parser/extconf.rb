ef099528d3d0dc92d29ac92df7975517956f66a0
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2017-08-13 23:04:59 -0400

    Continue making Travis happy

--- ext/graphql-idl-parser/extconf.rb
+++ ext/graphql-idl-parser/extconf.rb
@@ -8,31 +8,24 @@ INCLUDEDIR   = RbConfig::CONFIG['includedir']
 EXT_DIR      = File.expand_path(File.join(File.dirname(__FILE__), 'graphql-idl-parser'))
 FFI_DIR      = File.expand_path(File.join(EXT_DIR, 'graphql-idl-parser-ffi'))
 HEADER_DIR   = File.expand_path(File.join(EXT_DIR, 'graphql-idl-parser-ffi', 'includes'))
-RELEASE_DIR  = File.join(FFI_DIR, 'target', 'release')
 
-def ext
-  host_os = RbConfig::CONFIG['host_os']
-  case host_os
-  when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-    '.dll'
-  when /darwin|mac os/
-    '.dylib'
-  when /linux/
-    '.so'
-  when /solaris|bsd/
-    '.so'
-  else
-    raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
-  end
+if ENV['TRAVIS']
+  type = 'debug'
+  target = ''
+  flag = '-O0'
+else
+  type = 'release'
+  target = '--release'
+  flag = '-O2'
 end
+
+RELEASE_DIR  = File.join(FFI_DIR, 'target', type)
 
 if !system('cargo --version') || !system('rustc --version')
   raise '\n\n\n[ERROR]: You have to install Rust with Cargo (https://www.rust-lang.org/)'
 end
 
-type = ENV['TRAVIS'] ? '' : '--release'
-
-`cargo build #{type} --manifest-path #{File.join(FFI_DIR, 'Cargo.toml')}`
+`cargo build #{target} --manifest-path #{File.join(FFI_DIR, 'Cargo.toml')}`
 
 $LIBS << ' -lgraphqlidlparser'
 
@@ -40,8 +33,6 @@ HEADER_DIRS = [INCLUDEDIR, HEADER_DIR]
 LIB_DIRS = [LIBDIR, RELEASE_DIR]
 
 dir_config('graphql-idl-parser', HEADER_DIRS, LIB_DIRS)
-
-flag = ENV['TRAVIS'] ? '-O0' : '-O2'
 
 $CFLAGS << " #{flag} -std=c11 -Wall -pedantic -Werror -Wfatal-errors -Wstrict-aliasing"
 
